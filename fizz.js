f024a9976be16e8b452990fda97303d725203b68
JHokum <jdelgado92@hotmail.com>
2022-01-26 10:43:46 -0800

    Test script's connection to index.  Establish framework for script.

---
+++ fizz.js
@@ -0,0 +1,16 @@
+alert("Script connection sucess");
+
+/* Take in number from user, print all numbers from one to user's number
+   and if number is multiple of 3, output "Fizz"
+   Multiple of 5 ,output "Buzz"
+   Multiple of 3 and 5, output "FizzBuss"*/
+
+   //Prompt for initializing test variable, accepting user input
+
+   //function for test of variable, multiple of 3
+   
+   //function for test test of variable, multiple of 5
+
+   //function for test of variable, multiple of 3 and 5
+
+   //function to print result based on tests
