a128d3c22223f53909abfdc265f4c7be5bf8deea
JHokum <jdelgado92@hotmail.com>
2022-01-26 10:58:24 -0800

    Functions added to test user input and print results based on test of input. User input is not currently tested for integer compatibility.

--- fizz.js
+++ fizz.js
@@ -7,10 +7,29 @@ alert("Script connection sucess");
 
    //Prompt for initializing test variable, accepting user input
 
-   //function for test of variable, multiple of 3
+   test = prompt("Please enter a number."); //add switch to test if input is number
+   //alert(test);
+
    
+
+   //function for test of variable, multiple of 3
+   let multipleThree = test => test % 3 == 0;
    //function for test test of variable, multiple of 5
-
+   let multipleFive = test => test % 5 == 0;
    //function for test of variable, multiple of 3 and 5
-
+   let multipleThreeAndFive = test => test % 3 == 0 && test % 5==0;
    //function to print result based on tests
+   for (let i = 0; i <test; i++){
+        if (multipleThreeAndFive(i+1)){
+            console.log("FizzBuzz");
+        }
+        else if (multipleThree(i+1)){
+            console.log("Fizz");
+        }
+        else if (multipleFive(i+1)){
+            console.log("Buzz");
+        }
+        else{
+            console.log(i+1);
+        }
+   }
